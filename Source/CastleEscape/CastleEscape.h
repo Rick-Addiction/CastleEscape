ac61e7660d48a0fa9cf92c70e9a7d17fda137848
Rick-Addiction <henrique.rachti@gmail.com>
2020-11-05 00:07:53 -0300

    Some refactors to start the Gems Puzzle creation

--- Source/CastleEscape/CastleEscape.h
+++ Source/CastleEscape/CastleEscape.h
@@ -3,37 +3,4 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Engine/TriggerVolume.h"
-#include "CastleEscape.generated.h"
 
-UENUM(BlueprintType)
-enum class EPuzzleRotation : uint8 {
-
-    UP UMETA(DisplayName = "UP"),
-    LEFT UMETA(DisplayName = "LEFT"),
-    DOWN UMETA(DisplayName = "DOWN"),    
-    RIGHT UMETA(DisplayName = "RIGHT")
-    
-};
-
-USTRUCT(BlueprintType)
-struct FPuzzleSpace
-{
-    GENERATED_BODY();
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-        ATriggerVolume* PuzzleSpace;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-        AActor* PuzzlePieceExpected;
-
-    AActor* CurrentPuzzlePiecePlaced;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-        EPuzzleRotation PuzzlePieceRotationExpected;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    EPuzzleRotation CurrentPuzzlePieceRotation;
-
-    bool bAvailable = true;
-};
